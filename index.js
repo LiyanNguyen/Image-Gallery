aa9707e56268416301480c305111019931157311
LiyanNguyen <50958126+LiyanNguyen@users.noreply.github.com>
2022-02-17 10:44:19 +0800

    Update
    - added search filter functions
    - search image by either keywords, category, or file type
    - improve css performance

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let blocker = document.querySelector('.dark-blocker');
 let popupImageContainer = document.querySelector('.popup-image-container');
 let popupImage = document.querySelector('.popup-img');
 let images = document.querySelectorAll('.img');
+let searchBox = document.querySelector('#search-box');
+let categoryButton = document.querySelectorAll(".category-btn");
+let typeButton = document.querySelectorAll(".type-btn");
 
 //======= FUNCTION ON CLICKING MENU ON MOBILE =======//
 menu.onclick = () => {
@@ -43,10 +46,72 @@ images.forEach((img) => {
 	}
 });
 
-//======= FUNCTION TO SEARCH BY FILTERS =======//
+//======= FUNCTION TO SEARCH BY KEYWORDS =======//
+searchBox.oninput = () => {
+	typeButton.forEach((otherBtn) => {otherBtn.classList.remove("active");});
+	categoryButton.forEach((otherBtn) => {otherBtn.classList.remove("active");});
+	
+	let searchValue = searchBox.value.toLowerCase();
+	//compare the Searchvalue to every image's keywords
+	images.forEach((img) => {
+		let dataSearch = img.getAttribute('img-keywords');
+		//if the current data search value is in the image's keywords, we display them
+		if (dataSearch.indexOf(searchValue) != -1) {img.style.display = 'inline-block';}
+		//else we hide them
+		else {img.style.display = 'none';}
+	});
+};
 
+//======= FUNCTION TO SEARCH BY CATEGORY =======//
+categoryButton.forEach((btn) => {
+	btn.onclick = () => {
+		typeButton.forEach((otherBtn) => {otherBtn.classList.remove("active");});
+		categoryButton.forEach((otherBtn) => {otherBtn.classList.remove("active");});
+		searchBox.value = '';
+		
+		//then take the img-category value of the button
+		let dataCategory = btn.getAttribute('img-category');
+		//then compare the img-category value to the img-cat value of every image
+		images.forEach((img) => {
+			let dataCat = img.getAttribute("img-cat"); //we take the img-cat value of image
+			//if img-category value of the button is all, show all image
+			if (dataCategory === "all") {img.style.display = "inline-block";}
+			//if the value of img-category is the same as img-cat value of the image, show it
+			else if (dataCategory === dataCat) {img.style.display = "inline-block";}
+			//if not, hide it
+			else {img.style.display = "none";}
+		});
+		//add active class to the button currently selected
+		btn.classList.add('active');
+	};
+});
 
-//======= FUNCTION TO WAI FOR ENTIRE FILES TO PARSE FIRST =======//
+//======= FUNCTION TO SEARCH BY FILE TYPE =======//
+typeButton.forEach((btn) => {
+	btn.onclick = () => {
+		typeButton.forEach((otherBtn) => {otherBtn.classList.remove("active");});
+		categoryButton.forEach((otherBtn) => {otherBtn.classList.remove("active");});
+		searchBox.value = "";
+		
+		//then take the img-type value of the button
+		let dataType = btn.getAttribute("img-type");
+		//then we compare the img-type value to the img-cat value of every image
+		images.forEach((img) => {
+			//seperate file name via . then pop() to take the file extension type
+			let ImgType = img.getAttribute("src").split(".").pop();
+			//if img-type value of the button is all, show all image
+			if (dataType === "all") {img.style.display = "inline-block";}
+			//if the value of img-type is the same as img-cat value of the image, show it
+			else if (dataType === ImgType) {img.style.display = "inline-block";}
+			//if not, hide it
+			else {img.style.display = "none";}
+		});
+		//add active class to the button currently selected
+		btn.classList.add("active");
+	};
+});
+
+//======= FUNCTION TO WAIT FOR ENTIRE FILES TO PARSE FIRST =======//
 let checkIfDocumentFullyLoaded = () => {
 	if (document.readyState === "complete") {
 		// make the entire page visible
